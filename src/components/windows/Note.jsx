a653856a2287c1ccae4cf80b882b8352ea3a9ceb
mohitkumar7895 <mohitporwal596@gmail.com>
2026-01-21 12:47:55 +0530

    changes

--- src/components/windows/Note.jsx
+++ src/components/windows/Note.jsx
@@ -3,7 +3,7 @@ import Markdown from 'react-markdown'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atelierDuneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import MacWindow from './MacWindow'
-import "./note.scss"
+import "./Note.scss"
 
 
 const Note = ({ windowName, setWindowsState }) => {
